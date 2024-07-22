47373aae6f3786cfbd0dbaf3b7d06348961607ed
mehul <mehul192003@gmail.com>
2024-07-22 19:21:06 +0530

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logOutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logOutUser, registerUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetail, updateUserAvatar, updateUsercoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyToken } from "../middlewares/auth.middleware.js";
 
@@ -26,6 +26,23 @@ userRouter.route("/login").post(loginUser)
 userRouter.route("/logout").post(verifyToken,logOutUser)
 
 userRouter.route("/refresh-Token").post(refreshAccessToken)
+
+userRouter.route("/change-password").post(verifyToken,changeCurrentPassword)
+
+userRouter.route("/current-user").get(verifyToken,getCurrentUser)
+
+userRouter.route("/update-account").patch(verifyToken,updateAccountDetail)
+
+
+userRouter.route("/avatar").patch(verifyToken,upload.single("avatar"),updateUserAvatar)
+
+
+userRouter.route("/cover-Image").patch(verifyToken,upload.single("/coverImage"),updateUsercoverImage)
+
+userRouter.route("/c/:username").get(verifyToken,getUserChannelProfile)
+
+userRouter.route("/history").get(verifyToken,getWatchHistory)
+
 
 
 export  default userRouter ;
